5133f7c46cf9f5ddd81b6a2cbd7df5f2a9c51bf4
EugenBonta <e.bonta03@gmail.com>
2024-05-13 17:47:47 +0300

    CarRentService.java, BikeRentService.java

--- src/main/java/org/example/fancy_project/service/bike/BikeRentService.java
+++ src/main/java/org/example/fancy_project/service/bike/BikeRentService.java
@@ -1,26 +1,50 @@
 package org.example.fancy_project.service.bike;
 
+import org.example.fancy_project.classes.bike.Bike;
 import org.example.fancy_project.classes.bike.BikeRent;
+import org.example.fancy_project.dao.BikeDao;
 import org.example.fancy_project.dao.BikeRentDao;
 import org.example.fancy_project.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BikeRentService extends RentService<BikeRent> {
     @Autowired
     BikeRentDao bikeRentDao;
+    @Autowired
+    BikeDao bikeDao;
+
     public List<BikeRent> getAllRents() {
         return bikeRentDao.findAll();
     }
 
     public BikeRent createRent(BikeRent bikeRent){
+        bikeRent.setStartDate(getCurrentDateTimeString());
+        bikeRent.setFinishDate(null);
+        Optional<Bike> optionalBike = bikeDao.findById(bikeRent.getBike().getId());
+
+        if (optionalBike.isPresent()) {
+            Bike fetchedBike = optionalBike.get();
+            bikeRent.setBike(fetchedBike);
+        } else {
+            throw new IllegalArgumentException("BikeRent with ID " + bikeRent.getBike().getId() + " not found.");
+        }
+
         return bikeRentDao.save(bikeRent);
     }
-
     public void deleteRent(Integer id) {
         bikeRentDao.deleteById(id);
+    }
+
+    public static String getCurrentDateTimeString() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
     }
 }
--- src/main/java/org/example/fancy_project/service/car/CarRentService.java
+++ src/main/java/org/example/fancy_project/service/car/CarRentService.java
@@ -1,26 +1,51 @@
 package org.example.fancy_project.service.car;
 
+import org.example.fancy_project.classes.car.Car;
 import org.example.fancy_project.classes.car.CarRent;
+import org.example.fancy_project.dao.CarDao;
 import org.example.fancy_project.dao.CarRentDao;
 import org.example.fancy_project.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarRentService extends RentService<CarRent> {
     @Autowired
     CarRentDao carRentDao;
+    @Autowired
+    CarDao carDao;
+
     public List<CarRent> getAllRents() {
         return carRentDao.findAll();
     }
 
     public CarRent createRent(CarRent carRent){
+        carRent.setStartDate(getCurrentDateTimeString());
+        carRent.setFinishDate(null);
+        Optional<Car> optionalCar = carDao.findById(carRent.getCar().getId());
+
+        if (optionalCar.isPresent()) {
+            Car fetchedCar = optionalCar.get();
+            carRent.setCar(fetchedCar);
+        } else {
+            throw new IllegalArgumentException("CarRent with ID " + carRent.getCar().getId() + " not found.");
+        }
+
         return carRentDao.save(carRent);
     }
 
     public void deleteRent(Integer id) {
         carRentDao.deleteById(id);
+    }
+
+    public static String getCurrentDateTimeString() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
     }
 }
