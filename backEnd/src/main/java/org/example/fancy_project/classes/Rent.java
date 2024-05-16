0d6a831f54e87a191b3f8af1ea09e50390ded8e0
EugenBonta <e.bonta03@gmail.com>
2024-05-16 20:37:54 +0300

    backEnd, frontEnd

--- backEnd/src/main/java/org/example/fancy_project/classes/Rent.java
+++ backEnd/src/main/java/org/example/fancy_project/classes/Rent.java
@@ -1,6 +1,9 @@
 package org.example.fancy_project.classes;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.Getter;
 
