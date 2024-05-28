41f7e8ba9078dd8621e16997fcff5dec678bf2c1
sasa-bonta <bonta2000@mail.ru>
2024-05-28 14:17:28 +0300

    Cars skeleton loader

--- frontEnd/src/views/Cars.vue
+++ frontEnd/src/views/Cars.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { useCarsStore } from '@/stores/cars'
-import { computed, onMounted, reactive, type Ref, ref, watch } from 'vue'
-import { storeToRefs } from 'pinia'
-import { useDisplay } from 'vuetify'
+import {useCarsStore} from '@/stores/cars'
+import {computed, onMounted, reactive, type Ref, ref, watch} from 'vue'
+import {storeToRefs} from 'pinia'
+import {useDisplay} from 'vuetify'
 
 const store = useCarsStore()
-const { items, loading, error } = storeToRefs(store)
-const { fetchList } = store
+const {items, loading, error} = storeToRefs(store)
+const {fetchList} = store
 
 onMounted(() => {
   fetchList()
@@ -14,14 +14,14 @@ onMounted(() => {
 
 const cars = computed(() => {
   return items.value.map((obj) => {
-    return reactive({ ...obj, showDetails: false })
+    return reactive({...obj, showDetails: false})
   })
 })
 
 const list = [
-  { text: 'Real-Time', icon: 'mdi-clock' },
-  { text: 'Audience', icon: 'mdi-account' },
-  { text: 'Conversions', icon: 'mdi-flag' }
+  {text: 'Real-Time', icon: 'mdi-clock'},
+  {text: 'Audience', icon: 'mdi-account'},
+  {text: 'Conversions', icon: 'mdi-flag'}
 ]
 </script>
 
@@ -124,9 +124,9 @@ const list = [
           </v-row>
         </v-container>
 
-        <v-card-title> {{ car.brand }} {{ car.model }} </v-card-title>
+        <v-card-title> {{ car.brand }} {{ car.model }}</v-card-title>
 
-        <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+        <v-card-subtitle> 1,000 miles of wonder</v-card-subtitle>
 
         <v-card-actions>
           <v-btn color="orange-lighten-2" text="Rent now"></v-btn>
@@ -136,8 +136,8 @@ const list = [
           <v-spacer></v-spacer>
 
           <v-btn
-            :icon="car.showDetails ? 'mdi-chevron-up' : 'mdi-chevron-down'"
-            @click="car.showDetails = !car.showDetails"
+              :icon="car.showDetails ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="car.showDetails = !car.showDetails"
           ></v-btn>
         </v-card-actions>
 
@@ -154,7 +154,35 @@ const list = [
     </v-row>
   </v-container>
 
-  <div v-else>loading</div>
+  <v-container v-else>
+    <v-row v-for="n in 3" :key="n" class="my-4">
+      <v-card class="mx-auto" width="1280">
+        <v-container>
+          <v-row>
+            <v-col cols="3">
+              <v-skeleton-loader
+                  width="250"
+                  height="330"
+                  type="card, subtitle, actions"
+              ></v-skeleton-loader>
+            </v-col>
+            <v-col cols="3" v-for="i in 3" :key="i">
+              <v-skeleton-loader
+                  max-width="300"
+                  type="list-item"
+              ></v-skeleton-loader>
+              <v-skeleton-loader
+                  v-for="i in 3" :key="i"
+                  max-width="300"
+                  type="list-item-avatar"
+              ></v-skeleton-loader>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
